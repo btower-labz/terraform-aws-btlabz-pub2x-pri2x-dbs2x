75359e58610bd5d732fef4e8eda76b88ca5c1ca5
Roman Syrtsev <rvs@btower.net>
2020-10-22 17:30:08 +0000

    converted code to tf12, added nats

---
+++ var-rt-names.tf
@@ -0,0 +1,30 @@
+variable "rt_public_name" {
+  description = "Main public route table name."
+  type        = string
+  default     = "rt-public"
+}
+
+variable "rt_private_a_name" {
+  description = "Route table name for private subnet A."
+  type        = string
+  default     = "rt-private-a"
+}
+
+variable "rt_private_b_name" {
+  description = "Route table name for private subnet B."
+  type        = string
+  default     = "rt-private-b"
+}
+
+variable "rt_database_a_name" {
+  description = "Route table name for database subnet A."
+  type        = string
+  default     = "rt-database-a"
+}
+
+variable "rt_database_b_name" {
+  description = "Route table name for database subnet B."
+  type        = string
+  default     = "rt-database-b"
+}
+
